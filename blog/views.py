db918710a7e7035f86a530055346f421abf6fd49
OnlyOL <callofcthulhu123a@gmail.com>
2023-08-21 13:41:13 +0300

    add login and logout

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,28 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+
 # Create your views here.
 def index(request):
-    return render(request,'blog/index.html')
+    return render(request, 'blog/index.html')
+
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.success(request, "There Was An Error Logging In, Try Again...")
+            return redirect('login')
+    else:
+        return render(request, 'blog/login.html', {})
+
+def logout_user(request):
+    messages.success(request, "You logged out")
+    logout(request)
+    return redirect('home')
--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from blog.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index)
+    path('', index, name="home"),
+    path('', include('django.contrib.auth.urls')),
+    path('login_user/', login_user, name="login"),
+    path('logout_user/', logout_user, name="logout")
 ]
