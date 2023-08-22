10c17568a5f631f17533ccd0cdcfa63d7bae685c
OnlyOL <callofcthulhu123a@gmail.com>
2023-08-22 13:05:29 +0300

    add redirect for logined users

--- commentators/views.py
+++ commentators/views.py
@@ -5,18 +5,21 @@ from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def login_user(request):
-    if request.method == "POST":
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('home')
-        else:
-            messages.success(request, "There Was An Error Logging In, Try Again...")
-            return redirect('login')
+    if request.user.is_authenticated:
+        return redirect('home')
     else:
-        return render(request, 'login.html', {})
+        if request.method == "POST":
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.success(request, "There Was An Error Logging In, Try Again...")
+                return redirect('login')
+        else:
+            return render(request, 'login.html', {})
 
 
 def logout_user(request):
@@ -26,20 +29,23 @@ def logout_user(request):
 
 
 def register_user(request):
-    if request.method == "POST":
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password1']
-            email = form.cleaned_data['email']
-            user = authenticate(username=username, password=password, email=email)
-            login(request, user)
-            messages.success(request, ("Registration Successful!"))
-            return redirect('home')
+    if request.user.is_authenticated:
+        return redirect('home')
     else:
-        form = UserCreationForm()
+        if request.method == "POST":
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                username = form.cleaned_data['username']
+                password = form.cleaned_data['password1']
+                email = form.cleaned_data['email']
+                user = authenticate(username=username, password=password, email=email)
+                login(request, user)
+                messages.success(request, ("Registration Successful!"))
+                return redirect('home')
+        else:
+            form = UserCreationForm()
 
-    return render(request, 'register.html', {
-        'form': form,
-    })
+        return render(request, 'register.html', {
+            'form': form,
+        })
