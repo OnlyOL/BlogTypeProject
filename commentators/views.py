603f920cbf18de927b05337d5f24c3b9ab06b088
OnlyOL <callofcthulhu123a@gmail.com>
2023-08-22 16:59:42 +0300

    add custom form and refractor

--- commentators/views.py
+++ commentators/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from django.contrib.auth.forms import UserCreationForm
+from .forms import CustomUserCreationForm
 
 # Create your views here.
 def login_user(request):
@@ -33,7 +33,7 @@ def register_user(request):
         return redirect('home')
     else:
         if request.method == "POST":
-            form = UserCreationForm(request.POST)
+            form = CustomUserCreationForm(request.POST)
             if form.is_valid():
                 form.save()
                 username = form.cleaned_data['username']
@@ -44,7 +44,7 @@ def register_user(request):
                 messages.success(request, ("Registration Successful!"))
                 return redirect('home')
         else:
-            form = UserCreationForm()
+            form = CustomUserCreationForm()
 
         return render(request, 'register.html', {
             'form': form,
