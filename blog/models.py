6b0cb5ea14088709ebe140931a9fc7204de72df2
OnlyOL <callofcthulhu123a@gmail.com>
2023-08-25 17:25:52 +0300

    Post model

--- blog/models.py
+++ blog/models.py
@@ -1,2 +1,17 @@
 from django.db import models
-# Create your models here.
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    Image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title + ' | ' + str(self.author)
+
+    # Create your models here.
+    def get_absolute_url(self):
+        return reverse('article-detail', args=(str(self.id)))
