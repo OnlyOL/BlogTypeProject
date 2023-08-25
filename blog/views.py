ec69c30cef2598de4a811d3fec5cef0064536e4c
OnlyOL <callofcthulhu123a@gmail.com>
2023-08-25 17:27:52 +0300

    moving to class base view and show Posts/Posts forms

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render, HttpResponse, redirect
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post
+from django.urls  import reverse_lazy
 
 
 # Create your views here.
-def index(request):
-    return render(request, 'blog/index.html')
+class HomeView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
 
 
+class ArticleDetailView(DetailView):
+    model = Post
+    template_name = 'blog/article_detailed.html'
+
+
+class AddPostView(CreateView):
+    model = Post
+    template_name = 'blog/add_post.html'
+    fields = '__all__'
+
+
+class UpdatePostView(UpdateView):
+    model = Post
+    template_name = 'blog/update_post.html'
+    fields = ['title', 'body']
+
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = 'blog/delete_post.html'
+    success_url = reverse_lazy('home')
