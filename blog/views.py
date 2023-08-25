01fde68350b697a28a12cae4f689a7ee508564c6
OnlyOL <callofcthulhu123a@gmail.com>
2023-08-25 18:11:07 +0300

    show and order by post_date

--- blog/views.py
+++ blog/views.py
@@ -8,6 +8,7 @@ from django.urls  import reverse_lazy
 class HomeView(ListView):
     model = Post
     template_name = 'blog/home.html'
+    ordering = ['-post_date']
 
 
 class ArticleDetailView(DetailView):
