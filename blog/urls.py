8917d5ffefc872dff01504c4668a68aeeddea9f3
OnlyOL <callofcthulhu123a@gmail.com>
2023-08-28 19:29:33 +0300

    quick fix

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework import  routers
 from .views import *
 router = routers.DefaultRouter()
-router.register(r'articles', BlogViewSet)
+router.register(r'articles', BlogViewSet, basename='post')
 
 
 urlpatterns = [
